120513142ec52266bcc7bbcaaf02678d2b2ab6c5
spancavil <soancavil@gmail.com>
2022-07-12 10:05:03 -0300

    Template clase 5

---
+++ Clase 5 - Componentes II/template-clase5/src/components/NavBar/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from '../Button';
+import CartWidget from '../CartWidget';
+import './styles.css';
+
+export default function NavBar({temperatura, opcionAdicional}) {
+
+    //const {temperatura, opcionAdicional} = props;
+    //Prop es un objeto
+
+    return (
+        <ul>
+            <li><a className="active" href="#home">Home</a></li>
+            <li><a href="#news">Ropa masculina</a></li>
+            <li><a href="#contact">Ropa femenina</a></li>
+            <li><a href="#about">Accesorios</a></li>
+            <li><a href={`#${opcionAdicional}`}>{opcionAdicional}</a></li>
+            <CartWidget/>
+        </ul>
+    )
+} 
