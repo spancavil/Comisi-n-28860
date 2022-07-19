0873a8c8cf76652f994e8bc416482abe9759f107
spancavil <soancavil@gmail.com>
2022-07-19 11:18:52 -0300

    Desarrollo clase 7

--- Clase 7 - API/template-clase7/src/container/ItemListContainer/index.jsx
+++ Clase 7 - API/template-clase7/src/container/ItemListContainer/index.jsx
@@ -1,26 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ItemList from '../../components/ItemList';
 import { productosRaw } from '../../mocks/productos';
 
 const ItemListContainer = ({greeting}) => {
 
   const [productos, setProductos] = useState([])
+
+  const [pokemons, setPokemons] = useState([])
   
-  const traerProductos = new Promise ((accept, reject)=> {
+  /* const traerProductos = new Promise ((accept, reject)=> {
     setTimeout(()=> {
       accept(productosRaw)
     }, 3000)
   })
+ */
 
-  traerProductos
-  .then(result => {
-    setProductos(result)
-  })
-  .catch(error => {
-    alert(`Hubo un error: ${error}`)
-  })
+  //Queremos que la petición a la API se ejecute cuando montamos el componente
+  useEffect(()=> {
 
-  console.log(productos);
+    //La petición debe estar en una función asíncrona
+    const getPokemons = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+        const data = await response.json();
+        console.log(data);
+        setPokemons(data.results);
+      } catch (error) {
+        alert(`Hubo un error: ${error.message}`)
+      }
+    }
+    
+    getPokemons();
+
+  }, [])
+
+  console.log(pokemons);
 
   //Siempre que tengamos un array y queramos renderizar el contenido de cada elemento,
   //vamos a utilizar el método MAP.
